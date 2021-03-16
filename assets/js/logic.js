43034f40de7fc3106c7938658d330a39780356ed
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-03-16 16:48:58 -0500

    timer added

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,12 @@
-// TEST CODES!!!!!!!!!!
+// variables
 var startBtnEl = document.querySelector("#start-btn");
 var homePageEl = document.querySelector("#home-page");
 var choicesEl = document.querySelector(".buttons");
-var questionPEl = document.querySelector(".questionP");
-
+var questionPEl = document.querySelector("#questionsP");
 var indexQ = 0;
+var isCorrect = "Correct Answer";
+var isWrong = "Wrong Answer. 10 seconds have been deducted from your timer";
+
 // console.log(questionsEl);
 // console.log(questionsArray[0].correct);
 
@@ -13,16 +15,27 @@ var startQuiz = function () {
 
   homePageEl.style.margin = "5px 35%";
 
+  // timer();
   questions();
 };
+
+var count = 75;
+var interval = setInterval(function () {
+  document.getElementById("countdown").innerHTML = count;
+  count--;
+  if (count === 0) {
+    clearInterval(interval);
+    document.getElementById("countdown").innerHTML = "Done";
+    // or...
+    alert("You're out of time!");
+  }
+}, 1000);
 
 var questions = function () {
   var currentQuestion = questionsArray[indexQ];
   var titleEl = document.getElementById("questionTitle");
   titleEl.textContent = currentQuestion.question;
   titleEl.setAttribute("style", "margin-bottom: 25px");
-  // console.log(titleEl.textContent);
-  // console.log(currentQuestion);
 
   choicesEl.innerHTML = "";
 
@@ -39,17 +52,20 @@ var questions = function () {
 
 function questionClicker() {
   var correctAnswer = questionsArray[indexQ].correct;
-  // console.log(andwe);
   // if statement for when the user clicks the correct button
+
   if (correctAnswer === this.value && indexQ < questionsArray.length - 1) {
-    console.log("it was clicked");
-    indexQ++;
-    questions();
+    alert(isCorrect);
+  } else {
+    alert(isWrong);
+    count -= 10;
   }
+  indexQ++;
+  questions();
 }
 
 // event listener to start quiz
 startBtnEl.addEventListener("click", startQuiz);
 
 // event listener to all buttons
-choicesEl.addEventListener("click", questionClicker);
+// choicesEl.addEventListener("click", questionClicker);
